94d110f7bb59d7a2125f4ea22f6b35c19023ff1d
M <mrgmckenzie@gmail.com>
2023-04-10 15:36:31 +0100

    Completed Part A

---
+++ student_class/src/student.py
@@ -0,0 +1,11 @@
+
+class Student:
+    def __init__(self, input_name, input_cohort):
+        self.name = input_name
+        self.cohort = input_cohort
+    
+    def talk(self):
+        return "I can talk!"
+    
+    def say_favourite_language(self, input_language):
+        return f"I love {input_language}"
